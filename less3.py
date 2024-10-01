b69e3701238b83c08279fae021798382a5fc33d4
Deniz-22 <mrdenissemin@gmail.com>
2024-10-01 16:34:07 +0300

    Add files via upload
    Переменные

---
+++ less3.py
@@ -0,0 +1,6 @@
+number_lesson =  12 #Количество выполненных ДЗ
+number_hour = 1.5 #Количество затраченных часов
+name_course = "Python" #Название курса
+time_to_lesson = (number_hour/number_lesson) #Время на одно задание
+print('Курс:',name_course,', всего задач:',number_lesson,", затрачено часов:",number_hour, ', среднее время выполнения', time_to_lesson,'часов.' )
+
