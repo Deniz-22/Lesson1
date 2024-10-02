5783c1a440205f63636c679c4bc7efa83cded339
Deniz-22 <mrdenissemin@gmail.com>
2024-10-02 17:45:17 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var_ = 1, 2.0, 'string'
+print(immutable_var_)
+#immutable_var_[0] = True #элементы кортежа не изменны
+mutable_list_ = [1, 2, 3]
+mutable_list_[0] = True
+print(mutable_list_)
